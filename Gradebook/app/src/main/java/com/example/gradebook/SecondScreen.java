eed45a7812c93ac42ff86f79da09469a1b0b9973
Francisco Ramirez <cisco95@csu.fullerton.edu>
2020-02-26 10:23:09 -0800

    Entire Project Folder

---
+++ Gradebook/app/src/main/java/com/example/gradebook/SecondScreen.java
@@ -0,0 +1,13 @@
+package com.example.gradebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_screen);
+    }
+}
