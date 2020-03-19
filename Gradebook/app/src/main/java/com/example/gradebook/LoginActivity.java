c6aa9d48a4884bbdec84d9f5f645f6eb57a5b8c9
hqureshi434 <hqureshi434@gmail.com>
2020-03-19 14:13:47 -0700

    Added the LoginActivity.java file in order to organize the code by the Layout XML files. Cannot set the variables in LoginActivity.java for unknown reasons.

---
+++ Gradebook/app/src/main/java/com/example/gradebook/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.gradebook;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends Activity {
+    private EditText Email;
+    private EditText Password;
+    private Button Login;
+
+    Email = (EditText)findViewById(R.id.etEmail);
+}
